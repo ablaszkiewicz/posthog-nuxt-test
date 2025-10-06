d06fd15f8b4ab4507036b9ff975ae89ec1bbe793
ablaszkiewicz <kqmdjc8@gmail.com>
2025-10-06 16:17:26 +0200

    feat: wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,29 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@ablaszkiewicz/posthog-nuxt"],
+  modules: ["@posthog/nuxt"],
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  sourcemap: { client: "hidden" },
+  sourcemap: { client: true },
+  nitro: {
+    rollupConfig: {
+      output: {
+        sourcemapExcludeSources: false,
+      },
+    },
+  },
   posthog: {
     host: "http://localhost:8010",
     publicApiKey: "phc_VXlGk6yOu3agIn0h7lTmSOECAGWCtJonUJDAN4CexlJ",
-    exceptionAutoCaptureEnabled: true,
+    nuxt: {
+      exceptionAutoCaptureEnabled: true,
+    },
+    nitro: {
+      exceptionAutoCaptureEnabled: true,
+    },
     sourceMaps: {
       enabled: true,
+      version: "V1",
       envId: "2",
+      project: "i-love-nuxt-1",
       privateApiKey: "phx_YZZHl8xzLkCWHSpVahmkggLGaS6gmSxCNmH26N0RUGZnqAs",
     },
   },
