57b644e2689315c7cb5d1a1a3084efe745e378ec
ablaszkiewicz <kqmdjc8@gmail.com>
2025-10-09 14:36:21 +0200

    feat: wip

--- README.md
+++
@@ -1,1 +0,0 @@
-# posthog-nuxt-test
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
+import * as dupa from "@posthog/nuxt";
+
 export default defineNuxtConfig({
   modules: ["@posthog/nuxt"],
-  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+
   sourcemap: { client: true },
+
   nitro: {
     rollupConfig: {
       output: {
@@ -10,21 +13,24 @@ export default defineNuxtConfig({
       },
     },
   },
-  posthog: {
+
+  posthogConfig: {
     host: "http://localhost:8010",
-    publicApiKey: "phc_VXlGk6yOu3agIn0h7lTmSOECAGWCtJonUJDAN4CexlJ",
+    publicKey: "phc_VXlGk6yOu3agIn0h7lTmSOECAGWCtJonUJDAN4CexlJ",
     nuxt: {
       exceptionAutoCaptureEnabled: true,
+      debug: true,
     },
     nitro: {
       exceptionAutoCaptureEnabled: true,
+      debug: true,
     },
-    sourceMaps: {
+    sourcemaps: {
       enabled: true,
       version: "V1",
       envId: "2",
       project: "i-love-nuxt-1",
-      privateApiKey: "phx_YZZHl8xzLkCWHSpVahmkggLGaS6gmSxCNmH26N0RUGZnqAs",
+      personalApiKey: "phx_YZZHl8xzLkCWHSpVahmkggLGaS6gmSxCNmH26N0RUGZnqAs",
     },
   },
 });
